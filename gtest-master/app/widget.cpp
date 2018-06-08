825772bf7b4b0e2917d0a46e1d7893938aa30fe5
Denis Korolevskiy <korolevs@cs.karelia.ru>
2018-06-08 12:17:13 +0300

    Update widget.cpp

--- gtest-master/app/widget.cpp
+++ gtest-master/app/widget.cpp
@@ -5,13 +5,10 @@ Widget::Widget(int x, int y) :QWidget(0),ui(new Ui::Widget),
     x(x), y(y), iterations(0), scene(NULL)
 
 {
-    ui->setupUi(this); // Функция для выставки на нашем виджете GUI из widget.ui. Автогенерируемая
+    ui->setupUi(this); 
     timer = new QTimer();
     timer->setInterval(500);
-    /* Соединяем сигнал таймера о одном тике с слотом(функцией) on_timer_timeout */
     QObject::connect(timer,SIGNAL(timeout()),this,SLOT(on_timer_timeout()));
-
-    // Таймер не запускаем!
 }
 
 Widget::~Widget()
@@ -27,22 +24,18 @@ void Widget::on_pushButton_clicked()
 
 void Widget:: new_game(){
     if (scene == NULL) {// Если сцена еще не была создана
-        /* Создаем графическую сцена с размерами поля для вывода */
         scene = new QGraphicsScene(0,0,ui->graphicsView->viewport()->width(),ui->graphicsView->viewport()->height());
         fills.resize(x);//Мы будем обращаться к элементам через [][]. А он пуст. Поэтому надо сделать resize.
         /*Цикл обхода*/
         for (int i = 0; i < x; i++){
-            // Аналогичный resize. В итоге fills[i] - это вектор который хранит 8 элементов. На самом деле еще не хранит, но мы можем
-            //обращаться к этим эелментам как fills[i][j]
+       
             fills[i].resize(y);
 
             for (int j = 0; j < y; j++)
             {
                 /* Создаем LifeRect *, в положении 0-0 и размером зависящим от размера сцены! */
                 LifeRect * rec = new LifeRect(0,0,scene->sceneRect().width()/(x),scene->sceneRect().height()/(y));
-                //Добавляем на сцену наш элемент
                 scene->addItem(rec);
-                // А теперь после добавления выставляем его позицию! Позиция расчитывается относительно его номера и размера
                 rec->setPos( 0+ i*scene->width()/(x),0 + j* scene->height()/(y) );
                 /* "Добавляем" наш элемент в массив */
                 fills[i][j] = rec;
@@ -50,8 +43,7 @@ void Widget:: new_game(){
         }
     }
     else
-    { // Если сцена уже была создана - то просто останавливаем таймерам, обнуляем
-      // счетчик итераций, и делаем все клетки цвета циан
+    { 
 
         timer->stop();
         iterations = 0;
@@ -69,14 +61,14 @@ void Widget:: new_game(){
 
 
 void Widget::on_timer_timeout(){
-    int nebs ; // Переменная для хранения числа соседей
+    int nebs ; 
     QVector< LifeRect* > fill_temp; 
     for (int i = 0; i < x; i++){
 
         for (int j = 0; j < y; j++)
         {   nebs = count_neighbors(i,j);
             if (fills[i][j]->brush().color() == Qt::cyan && nebs == 3){
-                fill_temp.push_back(fills[i][j]);// Если соответствует - отправляем в вектор на перерисовку
+                fill_temp.push_back(fills[i][j]);
             }
             if (fills[i][j]->brush().color() == "#FF4500" && (nebs > 3 || nebs < 2)){
                 fill_temp.push_back(fills[i][j]);
@@ -85,16 +77,16 @@ void Widget::on_timer_timeout(){
     }
     if (fill_temp.count() == 0) // Если вектор с клетками на перерисовку пуст - заканчиваем игру!
     {
-        timer->stop();// Останавливаем таймер
+        timer->stop();
         QMessageBox::about(this, "GAME OVER!!!", "Iterations="+QString().setNum(iterations)); // Выводим сообщение с итерациями
-        iterations = 0;// Обнуляем количество итераций
+        iterations = 0;
     }
-    else // Если вектор не пуст - ничего не обнуляем
+    else 
     {   QVector< LifeRect* >::iterator it = fill_temp.begin();
         for (;it !=fill_temp.end();it++){
             (*it)->changeColor();
         }
-        iterations++;// ++ к числу итераций
+        iterations++;
     }
 
 }
@@ -116,11 +108,11 @@ int Widget::count_neighbors(int pos_x, int pos_y)
     int temp_j;
     int result = 0;
     for (i = pos_x -1 ; i <= pos_x + 1; i++) {
-        temp_i = i; // Вводятся временные переменные на случай если клетка граничная и сосед из другой стороны
+        temp_i = i; 
         if (i == -1) {temp_i = x-1;}
         else if (i == x) {temp_i = 0;}
         for (j = pos_y-1; j <= pos_y+1; j++) {
-            temp_j = j; // Вводятся временные переменные на случай если клетка граничная и сосед из другой стороны
+            temp_j = j;
             if (j == -1) {temp_j = y-1;}
             else if (j == y) {temp_j = 0;}
             if (fills[temp_i][temp_j]->brush().color() == "#FF4500" && !(temp_i == pos_x && temp_j == pos_y/* Если клетка - не мы*/))
@@ -128,6 +120,6 @@ int Widget::count_neighbors(int pos_x, int pos_y)
 
         }
     }
-    return result; // Возвращаем результат
+    return result; 
 }
 
